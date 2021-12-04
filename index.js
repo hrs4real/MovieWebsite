e7054068093b0f72c19688123ed209da22a49a0e
Harsh Srivastava <92949812+hrs4real@users.noreply.github.com>
2021-12-04 18:02:06 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const Low = () => {
-    document.getElementById('myImage').src='./bulb_off.gif';
-    document.getElementById('html_text').innerHTML = "LOW!!"
-}
-
-const High = () => {
-    document.getElementById('myImage').src='./bulbon.gif';
-    document.getElementById('html_text').innerHTML = "HIGH!!";
-}
